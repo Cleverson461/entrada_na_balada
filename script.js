20e3084e15083ed3d1a73ba1a31ca02ba9cf6abb
cleverson <cleverson461@gmail.com>
2024-03-13 23:22:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let idade = Number(prompt("Informe sua idade?"));
+
+if(idade >= 18){
+  document.write('Você está liberdado para entrar na Festa')
+} else{
+ document.write('NÃO liberado, você não tem a idade permitida!')
+}
